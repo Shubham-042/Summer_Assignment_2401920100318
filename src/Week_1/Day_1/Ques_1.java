001776919ea391a93900279fa6deb5d6b82b1024
Shubham-042 <shubhamkum042@gmail.com>
2026-06-02 11:39:09 +0530

    Initial commit

---
+++ src/Week_1/Day_1/Ques_1.java
@@ -0,0 +1,4 @@
+package Week_1.Day_1;
+
+public class Ques_1 {
+}
---
+++ src/Week_1/Day_1/Ques_3.java
@@ -0,0 +1,37 @@
+package Week_1.Day_1;
+
+public class Ques_3 {
+    public void rev(int[] arr,int i, int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int idx=-1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            rev(nums,0,n-1);
+            return;
+        }
+        for(int i=n-1; i>=idx; i--){
+            if(nums[i]>nums[idx]){
+                int temp=nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=temp;
+                break;
+            }
+        }
+        rev(nums,idx+1,n-1);
+
+    }
+}
