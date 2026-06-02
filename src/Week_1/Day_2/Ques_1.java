e298bae73c3671d681b4663684f7ee423e2840df
Shubham-042 <shubhamkum042@gmail.com>
2026-06-02 22:50:50 +0530

    Update Ques_1.java

--- src/Week_1/Day_2/Ques_1.java
+++ src/Week_1/Day_2/Ques_1.java
@@ -1,5 +1,17 @@
 package Week_1.Day_2;
 
 public class Ques_1 {
+public double findMaxAverage(int[] nums, int k) {
+        int currSum=0;
+        for(int i=0; i<k; i++){
+            currSum+=nums[i];
 
+        }
+        int maxSum=currSum;
+        for(int i=k; i<nums.length; i++){
+            currSum+=nums[i]-nums[i-k];
+            maxSum=Math.max(currSum,maxSum);
+        }
+        return (double)maxSum/k;
+    }
 }
