217fb732c8c74adb667480925d29b434909fb595
Shubham-042 <shubhamkum042@gmail.com>
2026-06-19 00:11:51 +0530

    week_3>Day_4

--- src/Week_3/Day_4/Ques_3.java
+++ src/Week_3/Day_4/Ques_3.java
@@ -1,4 +1,41 @@
 package Week_3.Day_4;
-
+import java.util.*;
 public class Ques_3 {
+    public int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int[] nse=new int[n];
+        int[] pse=new int[n];
+        Stack<Integer> st=new Stack<>();
+
+        // nse
+        for(int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && heights[i]<=heights[st.peek()])st.pop();
+
+            if(st.isEmpty()){
+                nse[i]=n;
+            }else{
+                nse[i]=st.peek();
+            }
+            st.push(i);
+        }
+        while(st.size()>0) st.pop();
+
+        // pse
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && heights[i]<=heights[st.peek()])st.pop();
+
+            if(st.isEmpty()){
+                pse[i]=-1;
+
+            }else{
+                pse[i]=st.peek();
+            }
+            st.push(i);
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            max=Math.max(max,(nse[i]-pse[i]-1)*heights[i]);
+        }
+        return max;
+    }
 }
