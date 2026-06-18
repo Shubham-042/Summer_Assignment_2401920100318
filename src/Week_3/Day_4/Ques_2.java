56101d34bd865a2b74981d124f67c5250b2fc44e
Shubham-042 <shubhamkum042@gmail.com>
2026-06-18 18:05:48 +0530

    week_3>Day_4

--- src/Week_3/Day_4/Ques_2.java
+++ src/Week_3/Day_4/Ques_2.java
@@ -1,4 +1,26 @@
 package Week_3.Day_4;
-
+import java.util.*;
 public class Ques_2 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack<>();
+        int ans=0;
+        for(int i=0; i<tokens.length; i++){
+            String op=tokens[i];
+            if(op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/")){
+
+                int v2=st.pop();
+                int v1=st.pop();
+                if(op.equals("+")) ans=v1+v2;
+                else if(op.equals("-")) ans=v1-v2;
+                else if(op.equals("*"))ans=v1*v2;
+                else ans=v1/v2;
+                st.push(ans);
+            }
+            else{
+                int num=Integer.parseInt(op);
+                st.push(num);
+            }
+        }
+        return st.peek();
+    }
 }
