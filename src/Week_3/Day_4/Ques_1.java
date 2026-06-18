0b663d73d74e8b2dbd7f30807e99ad2f7a3e7e7f
Shubham-042 <shubhamkum042@gmail.com>
2026-06-18 17:32:10 +0530

    week_3>Day_4

--- src/Week_3/Day_4/Ques_1.java
+++ src/Week_3/Day_4/Ques_1.java
@@ -1,4 +1,21 @@
 package Week_3.Day_4;
-
+import java.util.*;
 public class Ques_1 {
+    public int[] dailyTemperatures(int[] temp){
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[temp.length];
+        ans[temp.length-1]=0;
+        st.push(temp.length-1);
+        for(int i=temp.length-2; i>=0; i--){
+            while(!st.isEmpty() && temp[i]>=temp[st.peek()])st.pop();
+            if(!st.isEmpty() &&  temp[i]<temp[st.peek()]){
+                ans[i]=st.peek()-i;
+
+            }else{
+                ans[i]=0;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
 }
